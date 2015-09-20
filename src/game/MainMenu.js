0c1cd0c0b16e4a3e4a5baf4af091342b53cbaa0b
puzzud <puzzud@gmail.com>
2015-09-20 13:29:30 -0400

    Added tween example to title text.

--- src/game/MainMenu.js
+++ src/game/MainMenu.js
@@ -17,13 +17,21 @@ GameTitle.MainMenu.prototype.preload = function()
 GameTitle.MainMenu.prototype.create = function()
 {
   this.stage.backgroundColor = "#444444"; 
+
+  var titleTextX = this.world.centerX;
+  var titleTextY = ( this.world.height * ( 1 - 0.67 ) ) | 0;
   
-  var titleText = this.add.text( this.world.centerX, ( this.world.height * ( 1 - 0.67 ) ) | 0,
+  var titleText = this.add.text( this.world.centerX, titleTextY - 32,
                                  GameTitle.title, this.titleStyle );
 
   titleText.anchor.setTo( 0.5 );
 
   this.input.onDown.addOnce( this.startGame, this );
+
+  titleText.alpha = 0.0;
+  this.game.add.tween( titleText ).to( { alpha: 1 }, 500, Phaser.Easing.Linear.None, true );
+
+  this.game.add.tween( titleText ).to( { y: titleTextY }, 500, Phaser.Easing.Bounce.Out, true );
 };
 
 GameTitle.MainMenu.prototype.startGame = function()
