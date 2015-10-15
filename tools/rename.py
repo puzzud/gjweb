73d7b0cad6634a26442393f8c21e464e5b5faa0a
puzzud <puzzud@gmail.com>
2015-10-15 10:13:46 -0400

    Replaced rename.sh with rename.py.

--- tools/rename.py
+++ tools/rename.py
@@ -2,7 +2,6 @@
 
 import sys
 import os
-from glob import glob
 
 scriptName = sys.argv[0]
 
@@ -11,23 +10,19 @@ MIN_NUM_ARGUMENTS = 2
 numberOfArguments = len( sys.argv )
 if numberOfArguments - 1 < MIN_NUM_ARGUMENTS:
   print scriptName + " takes a minimum of " + str( MIN_NUM_ARGUMENTS ) + " arguments."
-  print scriptName + " /path/to/game/home NewGameName"
+  print "python " + scriptName + " /path/to/game/home NewGameName"
   print "Optionally, a nice name (which supports spaces) can be supplied."
-  print scriptName + " /path/to/game/home NewGameName \"New Game Name\""
+  print "python " + scriptName + " /path/to/game/home NewGameName \"New Game Name\""
   sys.exit( 1 )
 
-GAME_HOME = sys.argv[1]
-GAME_SAFE_NAME = sys.argv[2]
+GameHome = sys.argv[1]
+GameSafeName = sys.argv[2]
 
 if numberOfArguments - 1 > 2:
-  GAME_NICE_NAME = sys.argv[3]
+  GameNiceName = sys.argv[3]
 else:
-  GAME_NICE_NAME = ""
+  GameNiceName = ""
 
-# Find all the candidate files.
-#FILE_LIST=`find $GAME_HOME -type f | grep -v ".git" | grep -v "$GAME_HOME/tools"`
-#DIR_LIST=`find $GAME_HOME -type d | grep -v ".git" | grep -v "$GAME_HOME/tools"`
-  
 ############################
 def isPathValid( rootPath, path ):
   toolsPath = os.path.join( rootPath, "tools" )
@@ -65,58 +60,73 @@ def getValidDirs( rootPath ):
   return dirList
 
 ############################
-#def rename( rootPath, sourceString, replacementString ):
+def replaceStringsInFile( file, sourceString, replacementString ):
+  tempFileName = "out.txt"
+  
+  modifiedFile = False
+  
+  with open( tempFileName, "wt" ) as fileOut:
+    with open( file, "rt" ) as fileIn:
+      for line in fileIn:
+        newLine = line.replace( sourceString, replacementString )
+        if newLine != line:
+          modifiedFile = True
+          fileOut.write( newLine )
+        else:
+          fileOut.write( line )
+  
+  if modifiedFile:
+    os.remove( file )
+    os.rename( tempFileName, file )
+  else:
+    os.remove( tempFileName )
+    
+  return modifiedFile
 
-#function rename() {
-  #REPLACEMENT_PATTERN="s/$1/$2/g"
-  ##echo $REPLACEMENT_PATTERN
-  ##return 0;
+############################
+def rename( rootPath, sourceString, replacementString ):
+  fileList = getValidFiles( GameHome )
+  dirList  = getValidDirs( GameHome )
+  
+  print "*** Attempt to replace any references to " + sourceString + " in a file's contents."
+  for file in fileList:
+    #print file
+    
+    replaced = replaceStringsInFile( file, sourceString, replacementString )
+    if replaced:
+      print "Found at least one match in file contents:  " + file
+  print ""
+  
+  print "*** Attempt to rename any directories with " + sourceString + " in its name."
+  for dir in dirList:
+    #print dir
+    renamedDir = dir.replace( sourceString, replacementString );
+    
+    replaced = False
+    if renamedDir != dir:
+      replaced = True
+    if replaced:
+      os.rename( dir, renamedDir )
+      print "Found at least one match in directory name:  " + dir
+  print ""
+  
+  print "*** Attempt to rename any files with " + sourceString + " in its name."
+  for file in fileList:
+    #print file
+    renamedFile = file.replace( sourceString, replacementString );
+    
+    replaced = False
+    if renamedFile != file:
+      replaced = True
+    if replaced:
+      os.rename( file, renamedFile )
+      print "Found at least one match in file name:  " + file
+  print ""
 
-  #echo "Attempt to replace any references to $1 in its name."
-  #for f in $FILE_LIST
-  #do
-    ##echo $f
+# Rename references of files and in contents of files with specified arguments.
+rename( GameHome, "GameTitle", GameSafeName )
 
-    #sed -i -- "$REPLACEMENT_PATTERN" $f
-    #if [ "$?" == "0" ];
-    #then
-      #echo "Found at least one match in file contents:  $f"
-    #fi
-  #done
-  #echo ""
+if GameNiceName != "":
+  rename( GameHome, "Game Title", GameNiceName )
 
-  #echo "Attempt to rename any directories with $1 in its name."
-  #for d in $DIR_LIST
-  #do
-    ##echo $d
-
-    #RENAMED_DIR=`echo $d | sed -e "$REPLACEMENT_PATTERN"`
-    #if [ "$RENAMED_DIR" != "$d" ];
-    #then
-      #echo "Found at least one match in directory name:  $d"
-      #mv "$d" "$RENAMED_DIR"
-    #fi
-  #done
-  #echo ""
-
-  #echo "Attempt to rename any files with $1 in its name."
-  #for f in $FILE_LIST
-  #do
-    ##echo $f
-
-    #RENAMED_FILE=`echo $f | sed -e "$REPLACEMENT_PATTERN"`
-    #if [ "$RENAMED_FILE" != "$f" ];
-    #then
-      #echo "Found at least one match in file name:  $f"
-      #mv "$f" "$RENAMED_FILE"
-    #fi
-  #done
-  #echo ""
-#}
-
-#rename "GameTitle" "$GAME_SAFE_NAME"
-##rename "Game Title" "$GAME_NICE_NAME"
-
-#exit 0
-
-
+sys.exit( 0 )
