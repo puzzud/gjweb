b82616e2a54d6956eb84142d1405f2c64e229b2d
puzzud <puzzud@gmail.com>
2015-11-01 15:41:57 -0500

    #7 Added improved About screen showing contributors in a scrolling credits fashion.

--- src/game/About.js
+++ src/game/About.js
@@ -11,6 +11,10 @@ GameTitle.About = function( game )
 
   this.contributorListStyle = { font: "32px Arial", fill: "#ffffff" };
   this.contributorRowList = [];
+
+  this.creditsYPosition = 0;
+  this.creditsHeight = 0;
+  this.creditsTweenList = [];
 };
 
 GameTitle.About.stateKey = "About";
@@ -80,6 +84,18 @@ GameTitle.About.prototype.returnToMainMenu = function()
 
 GameTitle.About.prototype.setupAuthorText = function( textStartYPosition )
 {
+  this.setupCredits( textStartYPosition );
+  
+  this.createCreditsScrolling();
+  this.resetCreditsScrolling();
+
+  return textStartYPosition + this.creditsHeight;
+};
+
+GameTitle.About.prototype.setupCredits = function( textStartYPosition )
+{
+  this.creditsYPosition = textStartYPosition;
+
   if( GameTitle.projectInfo === null ||
       GameTitle.projectInfo.contributors === undefined )
   {
@@ -97,11 +113,11 @@ GameTitle.About.prototype.setupAuthorText = function( textStartYPosition )
   var nameColumnXPosition = gameHorizontalCenter - columnOffsetFromCenter;
   var contributionColumnXPosition = gameHorizontalCenter + columnOffsetFromCenter;
   
-  var labelYPosition = textStartYPosition;
+  var labelYPosition = this.creditsYPosition;
   
   var numberOfContributors = contributorList.length;
   var labelHeight = 48;
-  var contributorFieldHeightTotal = numberOfContributors * labelHeight;
+  this.creditsHeight = numberOfContributors * labelHeight;
   
   var contributor = null;
   for( var i = 0; i < numberOfContributors; i++ )
@@ -109,6 +125,7 @@ GameTitle.About.prototype.setupAuthorText = function( textStartYPosition )
     contributor = contributorList[i];
 
     var contributorRow = this.game.add.group();
+    contributorRow.position.setTo( 0, labelYPosition );
     this.contributorRowList.push( contributorRow );
 
     var labelName = this.game.add.text( 0, 0,
@@ -118,7 +135,7 @@ GameTitle.About.prototype.setupAuthorText = function( textStartYPosition )
     if( contributor.url !== undefined &&
         contributor.url !== "" )
     {
-      var urlButton = this.game.add.button( nameColumnXPosition, labelYPosition,
+      var urlButton = this.game.add.button( nameColumnXPosition, 0,
         null, this.navigateToContributorUrl, this );
 
       urlButton.addChild( labelName );
@@ -126,15 +143,14 @@ GameTitle.About.prototype.setupAuthorText = function( textStartYPosition )
     }
     else
     {
-      labelName.position.setTo( nameColumnXPosition, labelYPosition );
+      labelName.position.setTo( nameColumnXPosition, 0 );
       nameField = labelName;
     }
 
     labelName.anchor.setTo( 1.0, 0.5 );
-    labelName.tint = 0x888888;
     contributorRow.add( nameField );
 
-    var contributionField = this.game.add.text( contributionColumnXPosition, labelYPosition,
+    var contributionField = this.game.add.text( contributionColumnXPosition, 0,
       contributor.contribution, this.contributorListStyle );
     contributionField.anchor.setTo( 0, 0.5 );
     contributionField.tint = 0x888888;
@@ -142,8 +158,90 @@ GameTitle.About.prototype.setupAuthorText = function( textStartYPosition )
 
     labelYPosition += labelHeight;
   }
+};
 
-  return textStartYPosition + contributorFieldHeightTotal;
+GameTitle.About.prototype.createCreditsScrolling = function()
+{
+  if( GameTitle.projectInfo === null ||
+      GameTitle.projectInfo.contributors === undefined )
+  {
+    return;
+  }
+
+  this.resetCreditsScrolling();
+
+  var contributorList = GameTitle.projectInfo.contributors;
+
+  // Set up tweens to scroll and fade credits.
+  var labelHeight = 48;
+  var scrollDistance = 0;
+  var scrollTime = 0;
+  var scrollSpeed = 0.025;
+
+  var creditsBelowBottomYPosition = this.creditsYPosition + this.creditsHeight;
+  var creditsBottomYPosition = creditsBelowBottomYPosition - labelHeight;
+  var creditsAboveTopYPosition = this.creditsYPosition - labelHeight;
+
+  this.creditsTweenList.length = 0;
+
+  for( var i = 0; i < contributorList.length; i++ )
+  {
+    var contributorRow = this.contributorRowList[i];
+
+    var tween = this.game.add.tween( contributorRow );
+
+    scrollDistance = contributorRow.position.y - creditsBottomYPosition;
+    scrollTime = ( scrollDistance / scrollSpeed ) | 0;
+    tween.to( { alpha: 1.0, y: creditsBottomYPosition },
+      scrollTime, Phaser.Easing.Linear.None, false, scrollTime * i );
+
+    scrollDistance = creditsBottomYPosition - this.creditsYPosition;
+    scrollTime = ( scrollDistance / scrollSpeed ) | 0;
+    tween.to( { y: this.creditsYPosition },
+      scrollTime, Phaser.Easing.Linear.None, false );
+
+    scrollDistance = this.creditsYPosition - creditsAboveTopYPosition;
+    scrollTime = ( scrollDistance / scrollSpeed ) | 0;
+    tween.to( { alpha: 0.0, y: creditsAboveTopYPosition },
+      scrollTime, Phaser.Easing.Linear.None, false );
+    
+    this.creditsTweenList.push( tween );
+  }
+
+  // When the last credit goes out of view, restart scrolling.
+  var lastTween = this.creditsTweenList[this.creditsTweenList.length - 1];
+  if( lastTween !== undefined )
+  {
+    lastTween.onComplete.add( this.resetCreditsScrolling, this );
+  }
+};
+
+GameTitle.About.prototype.resetCreditsScrolling = function()
+{
+  if( GameTitle.projectInfo === null ||
+      GameTitle.projectInfo.contributors === undefined )
+  {
+    return;
+  }
+  
+  var contributorList = GameTitle.projectInfo.contributors;
+
+  var creditsBelowBottomYPosition = this.creditsYPosition + this.creditsHeight;
+
+  // Reset the position of the credits rows.
+  for( var i = 0; i < contributorList.length; i++ )
+  {
+    var contributorRow = this.contributorRowList[i];
+    
+    contributorRow.position.setTo( 0, creditsBelowBottomYPosition );
+    contributorRow.alpha = 0.0;
+  }
+
+  // Start tweens.
+  for( var tweenIndex = 0; tweenIndex < this.creditsTweenList.length; tweenIndex++ )
+  {
+    this.creditsTweenList[tweenIndex].start();
+  }
 };
 
 GameTitle.About.prototype.navigateToContributorUrl = function( button )
