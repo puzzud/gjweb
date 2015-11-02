8bee3ba1395240e74bfc39ab72704fa0f283612a
puzzud <puzzud@gmail.com>
2015-11-02 17:31:48 -0500

    Added npm command nwjs_build to build desktop builds of game. Dialed back grunt-nw-builder to use version 0.12.3 builds of the NW JS runtime.

--- tools/grunt/nwjs.js
+++ tools/grunt/nwjs.js
@@ -10,8 +10,10 @@ module.exports = function (grunt, tasks) {
   tasks.nwjs = {
     // The files that we want to check.
     options: {
-        platforms: ['win64', 'osx64', 'linux64'], // TODO: Pull these platforms from package.json somehow?
-        buildDir: grunt.uriBuild, // Where the build version of my NW.js app is saved.
+      version: "0.12.3", // NOTE: Need to use an old fixed version until grunt-nw-builder and nw-builder improve support.
+      platforms: ['win64', 'osx64', 'linux64'], // TODO: Pull these platforms from package.json somehow?
+      buildDir: grunt.uriBuild, // Where the build version of my NW.js app is saved.
+      cacheDir: grunt.uri + 'node_modules/grunt-nw-builder/cache'
     },
     src: [grunt.uri + 'www/**/*'] // NW.js app location.
   };
