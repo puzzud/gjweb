da249bd897cc01917c5115cb7e9bba51c2ad7a6d
puzzud <puzzud@gmail.com>
2015-11-02 09:34:39 -0500

    Expanded make_www.sh script and added npm script command cordova_build to build for currently configured platforms.

--- tools/make_www.sh
+++ tools/make_www.sh
@@ -1,1 +1,36 @@
-rm www -rf; mkdir www; cp assets -r www; cp src -r www; cp index.html www; cp style.css www; cp package.json www;
+onlyRemove=0
+
+for arg in "$@"
+do
+  if [ "$arg" == "rm" ]; then
+    onlyRemove=1
+  fi
+done
+
+# First, remove www directory.
+if [ -e "www" ]; then
+  echo "Removing www directory."
+  rm www -rf;
+
+  if [ "$?" -ne "0" ]; then
+    echo "Failed to remove www directory."
+    exit $?
+  fi
+
+  if [ "$onlyRemove" -eq "1" ]; then
+    exit 0
+  fi
+fi
+
+# Make www directory and copy over files.
+echo "Creating www directory."
+mkdir www;
+
+echo "Populating www directory."
+cp assets -r www;
+cp src -r www;
+cp index.html www;
+cp style.css www;
+cp package.json www;
+
+exit 0
